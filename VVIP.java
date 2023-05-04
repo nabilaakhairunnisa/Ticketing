aeac22463b6344db3ece7fcc11af1c3adf120cca
nabilaakhairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-05-04 21:11:37 +0700

    VVIP.java

--- VVIP.java
+++ VVIP.java
@@ -11,33 +11,42 @@ public class VVIP {
 
     public static int tiket;
 
-
     //METHOD pesanVVIP
     public static void pesanVVIP(){
         System.out.println("______________________________________________________________________");
         System.out.println("|                      Menu Pesan Tiket VVIP                         |");
         System.out.println("|____________________________________________________________________|");
         System.out.print  ("| Masukkan banyak tiket yang ingin dipesan: ");
-        tiket = input.nextInt();
-        banyakTiket.push(tiket);
-        totalTiket();
-        System.out.println(jumlah);
+        tiket = input.nextInt();     //menerima input nilai
+        banyakTiket.push(tiket);     //menyimpan nilai tiket ke dalam stack banyakTiket
+        totalTiket();                //memanggil method totalTiket untuk dijumlahkan
         System.out.println("| Total harga yang harus dibayar adalah: " + getTiket());
+        hargaTiket.push(getTiket()); //menyimpan nilai yang dihasilkan dari method getTiket ke dalam stack hargaTiket
+        totalHarga();               //memanggil method totalHarga untuk dijumlahkan
         System.out.println("|____________________________________________________________________|");
         System.out.println("|                  Terima Kasih Telah Memesan!                       |");
         System.out.println("|                     Selamat Menyaksikan :)                         |");
     }
 
-    public static int jumlah;
+    public static int banyak;
     public static void totalTiket(){
         while (!banyakTiket.isEmpty()){
             int elemen = banyakTiket.pop();
-            jumlah += elemen;
+            banyak += elemen;
         }
     }
 
     public static int getTiket() {
         int harga = 45000;
         return (tiket * harga);
+    }
+
+    //method totalHarga
+    public static int harga;
+    public static void totalHarga(){
+        while(!hargaTiket.isEmpty()){
+            int elemen = hargaTiket.pop();
+            harga += elemen;
+        }
     }
 }
