49132f8be4d05ff60c086c2bde40566331f0dbcc
nabilaakhairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-05-04 17:44:10 +0700

    VVIP.java

--- VVIP.java
+++ VVIP.java
@@ -1,21 +1,43 @@
 package TiketBioskop;
+
 import java.util.Scanner;
+import java.util.Stack;
+
 public class VVIP {
-    public void VVIP() {
-        Scanner input = new Scanner(System.in);
 
-        int jumlahTiket;
-        int totalHarga;
+    static Scanner input = new Scanner (System.in);
+    static Stack<Integer> banyakTiket = new Stack<Integer>();
+    static Stack<Integer> hargaTiket = new Stack<Integer>();
 
-        System.out.println("===============================================");
-        System.out.println("           Menu Ticketing Bioskop VVIP         ");
-        System.out.println("Masukkan jumlah tiket:                         ");
-        jumlahTiket = input.nextInt();
-        totalHarga = jumlahTiket * 45000;
-        System.out.println("Total Harga:                                  ");
-        System.out.println(totalHarga);
-        System.out.println("Terima kasih sudah membeli tiket di Cinema XXI");
-        System.out.println("Selamat Menyaksikan :)                        ");
-        System.out.println("===============================================");
+    public static int tiket;
+
+
+    //METHOD pesanVVIP
+    public static void pesanVVIP(){
+        System.out.println("______________________________________________________________________");
+        System.out.println("|                      Menu Pesan Tiket VVIP                         |");
+        System.out.println("|____________________________________________________________________|");
+        System.out.print  ("| Masukkan banyak tiket yang ingin dipesan: ");
+        tiket = input.nextInt();
+        banyakTiket.push(tiket);
+        totalTiket();
+        System.out.println(jumlah);
+        System.out.println("| Total harga yang harus dibayar adalah: " + getTiket());
+        System.out.println("|____________________________________________________________________|");
+        System.out.println("|                  Terima Kasih Telah Memesan!                       |");
+        System.out.println("|                     Selamat Menyaksikan :)                         |");
+    }
+
+    public static int jumlah;
+    public static void totalTiket(){
+        while (!banyakTiket.isEmpty()){
+            int elemen = banyakTiket.pop();
+            jumlah += elemen;
+        }
+    }
+
+    public static int getTiket() {
+        int harga = 45000;
+        return (tiket * harga);
     }
 }
